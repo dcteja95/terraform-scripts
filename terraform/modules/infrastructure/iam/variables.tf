54dd299dcc36c0ba76682addeb95891b6820eaf3
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:39:29 +0530

    feat: add iam variables

---
+++ terraform/modules/infrastructure/iam/variables.tf
@@ -0,0 +1,6 @@
+variable "name_prefix" { type=string }
+variable "raw_bucket_arn" { type=string }
+variable "curated_bucket_arn" { type=string }
+variable "artifacts_bucket_arn" { type=string }
+variable "kms_key_arn" { type=string }
+variable "tags" { type=map(string) }
