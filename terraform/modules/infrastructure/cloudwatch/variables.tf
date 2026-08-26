bf4a025ca5497c69082d0da40252c927e80973b9
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:48:10 +0530

    fix: add dashboard region input

--- terraform/modules/infrastructure/cloudwatch/variables.tf
+++ terraform/modules/infrastructure/cloudwatch/variables.tf
@@ -1,4 +1,5 @@
 variable "name_prefix" { type=string }
+variable "region" { type=string }
 variable "nat_gateway_ids" { type=list(string) }
 variable "sns_topic_arn" { type=string default="" }
 variable "tags" { type=map(string) }
