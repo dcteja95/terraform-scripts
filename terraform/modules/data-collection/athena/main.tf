e10753ebac85acced13c6a8a2a97a8f993749191
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:47:43 +0530

    feat: add curated Athena query

--- terraform/modules/data-collection/athena/main.tf
+++ terraform/modules/data-collection/athena/main.tf
@@ -1,1 +1,2 @@
 resource "aws_athena_workgroup" "this" { name="${var.name_prefix}-athena" configuration { enforce_workgroup_configuration=true result_configuration { output_location="s3://${var.artifacts_bucket_name}/athena-results/" } } tags=var.tags }
+resource "aws_athena_named_query" "curated_sample" { name="${var.name_prefix}-curated-sample" database=var.database_name workgroup=aws_athena_workgroup.this.name query="SELECT * FROM ${var.table_name} LIMIT 10;" description="Sample query against curated data" }
