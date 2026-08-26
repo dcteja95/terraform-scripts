bf481e9aa2289159097d54625d7b1ceb3048c229
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:47:17 +0530

    feat: add curated data glue crawler

--- terraform/modules/data-collection/glue/main.tf
+++ terraform/modules/data-collection/glue/main.tf
@@ -6,3 +6,4 @@ resource "aws_s3_object" "script" {
 }
 resource "aws_glue_catalog_database" "this" { name=replace("${var.name_prefix}_catalog","-","_") }
 resource "aws_glue_job" "transform" { name="${var.name_prefix}-glue-transform" role_arn=var.data_role_arn glue_version="4.0" worker_type="G.1X" number_of_workers=2 command { name="glueetl" script_location="s3://${var.artifacts_bucket_name}/${aws_s3_object.script.key}" python_version="3" } default_arguments={"--job-language"="python","--RAW_PATH"="s3://${var.raw_bucket_name}/bike-sharing/","--CURATED_PATH"="s3://${var.curated_bucket_name}/bike-sharing/"} tags=var.tags depends_on=[aws_s3_object.script] }
+resource "aws_glue_crawler" "curated" { name="${var.name_prefix}-curated-crawler" role=var.data_role_arn database_name=aws_glue_catalog_database.this.name table_prefix="bike_" s3_target { path="s3://${var.curated_bucket_name}/bike-sharing/" } }
