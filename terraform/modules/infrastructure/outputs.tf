dc4eeb51d5a68302649c03846e44ef7334d19416
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:40:59 +0530

    feat: add infrastructure outputs

---
+++ terraform/modules/infrastructure/outputs.tf
@@ -0,0 +1,10 @@
+output "vpc_id" { value=module.vpc.vpc_id }
+output "private_subnet_ids" { value=module.vpc.private_subnet_ids }
+output "raw_bucket_name" { value=module.s3.raw_bucket_name }
+output "curated_bucket_name" { value=module.s3.curated_bucket_name }
+output "artifacts_bucket_name" { value=module.s3.artifacts_bucket_name }
+output "kms_key_arn" { value=module.kms.kms_key_arn }
+output "data_role_arn" { value=module.iam.data_role_arn }
+output "ml_role_arn" { value=module.iam.ml_role_arn }
+output "sns_topic_arn" { value=module.sns.topic_arn }
+output "workload_security_group_id" { value=module.security_groups.workload_security_group_id }
