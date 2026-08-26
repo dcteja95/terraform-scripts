bf796851960f43a1b5438fcce87466d814b096bc
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:45:06 +0530

    fix: upload glue script through terraform

--- terraform/modules/data-collection/glue/main.tf
+++ terraform/modules/data-collection/glue/main.tf
@@ -1,2 +1,8 @@
+resource "aws_s3_object" "script" {
+  bucket = var.artifacts_bucket_name
+  key = "glue/glue_transform.py"
+  source = "${path.root}/../../../data/transformation/glue_transform.py"
+  etag = filemd5("${path.root}/../../../data/transformation/glue_transform.py")
+}
 resource "aws_glue_catalog_database" "this" { name=replace("${var.name_prefix}_catalog","-","_") }
-resource "aws_glue_job" "transform" { name="${var.name_prefix}-glue-transform" role_arn=var.data_role_arn glue_version="4.0" worker_type="G.1X" number_of_workers=2 command { name="glueetl" script_location="s3://${var.artifacts_bucket_name}/glue/glue_transform.py" python_version="3" } default_arguments={"--job-language"="python","--RAW_PATH"="s3://${var.raw_bucket_name}/bike-sharing/","--CURATED_PATH"="s3://${var.curated_bucket_name}/bike-sharing/"} tags=var.tags }
+resource "aws_glue_job" "transform" { name="${var.name_prefix}-glue-transform" role_arn=var.data_role_arn glue_version="4.0" worker_type="G.1X" number_of_workers=2 command { name="glueetl" script_location="s3://${var.artifacts_bucket_name}/${aws_s3_object.script.key}" python_version="3" } default_arguments={"--job-language"="python","--RAW_PATH"="s3://${var.raw_bucket_name}/bike-sharing/","--CURATED_PATH"="s3://${var.curated_bucket_name}/bike-sharing/"} tags=var.tags depends_on=[aws_s3_object.script] }
