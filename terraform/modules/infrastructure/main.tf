c55dd21dde1877f506de71dbd97c87f5445e937d
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:48:16 +0530

    fix: pass region to cloudwatch

--- terraform/modules/infrastructure/main.tf
+++ terraform/modules/infrastructure/main.tf
@@ -6,6 +6,6 @@ module "cloudtrail" { source="./cloudtrail" name_prefix=var.name_prefix tags=var
 module "flow_logs" { source="./flow-logs" name_prefix=var.name_prefix vpc_id=module.vpc.vpc_id tags=var.tags }
 module "sns" { source="./sns" name_prefix=var.name_prefix alert_email=var.alert_email tags=var.tags }
 module "budget" { source="./budget" name_prefix=var.name_prefix monthly_limit=var.monthly_budget_limit sns_topic_arn=var.enable_alerting ? module.sns.topic_arn : "" enabled=var.enable_budget }
-module "cloudwatch" { source="./cloudwatch" name_prefix=var.name_prefix nat_gateway_ids=module.vpc.nat_gateway_ids sns_topic_arn=var.enable_alerting ? module.sns.topic_arn : "" tags=var.tags }
+module "cloudwatch" { source="./cloudwatch" name_prefix=var.name_prefix region=var.region nat_gateway_ids=module.vpc.nat_gateway_ids sns_topic_arn=var.enable_alerting ? module.sns.topic_arn : "" tags=var.tags }
 module "security_groups" { source="./security-groups" name_prefix=var.name_prefix vpc_id=module.vpc.vpc_id tags=var.tags }
 module "evaluator_access" { source="./evaluator-access" name_prefix=var.name_prefix account_id=var.evaluator_account_id external_id=var.evaluator_external_id tags=var.tags }
