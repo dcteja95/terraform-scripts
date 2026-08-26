14e63445a390563c6aecd1026f191d8640af000b
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:42:45 +0530

    feat: add training variables

---
+++ terraform/modules/mlops/training/variables.tf
@@ -0,0 +1,8 @@
+variable "name_prefix" { type=string }
+variable "ml_role_arn" { type=string }
+variable "curated_bucket_name" { type=string }
+variable "artifacts_bucket_name" { type=string }
+variable "training_image_uri" { type=string }
+variable "instance_type" { type=string default="ml.m5.large" }
+variable "enable_training" { type=bool default=false }
+variable "tags" { type=map(string) }
