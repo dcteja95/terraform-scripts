f3c90daf34cf6d439bd676335e4460622713781e
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:43:57 +0530

    feat: add mlops outputs

---
+++ terraform/modules/mlops/outputs.tf
@@ -0,0 +1,3 @@
+output "model_package_group_name" { value=module.sagemaker.model_package_group_name }
+output "training_job_name" { value=module.training.training_job_name }
+output "endpoint_name" { value=module.endpoint.endpoint_name }
