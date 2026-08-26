4fa60705d685867c1878f106e4868431cff27a27
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:46:03 +0530

    feat: expose evaluator role

--- terraform/modules/infrastructure/outputs.tf
+++ terraform/modules/infrastructure/outputs.tf
@@ -1,5 +1,6 @@
 output "vpc_id" { value=module.vpc.vpc_id }
 output "private_subnet_ids" { value=module.vpc.private_subnet_ids }
+output "nat_gateway_ids" { value=module.vpc.nat_gateway_ids }
 output "raw_bucket_name" { value=module.s3.raw_bucket_name }
 output "curated_bucket_name" { value=module.s3.curated_bucket_name }
 output "artifacts_bucket_name" { value=module.s3.artifacts_bucket_name }
@@ -8,3 +9,4 @@ output "data_role_arn" { value=module.iam.data_role_arn }
 output "ml_role_arn" { value=module.iam.ml_role_arn }
 output "sns_topic_arn" { value=module.sns.topic_arn }
 output "workload_security_group_id" { value=module.security_groups.workload_security_group_id }
+output "evaluator_role_arn" { value=module.evaluator_access.role_arn }
