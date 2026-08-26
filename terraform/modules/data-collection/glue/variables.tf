547c9ba693966e3d7b039d2b7f44a818500cdd90
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:41:42 +0530

    feat: add glue variables

---
+++ terraform/modules/data-collection/glue/variables.tf
@@ -0,0 +1,6 @@
+variable "name_prefix" { type=string }
+variable "raw_bucket_name" { type=string }
+variable "curated_bucket_name" { type=string }
+variable "artifacts_bucket_name" { type=string }
+variable "data_role_arn" { type=string }
+variable "tags" { type=map(string) }
