6e940d9f92857c3d7e2cff61b54c61f489d6d840
dcteja95 <132430143+dcteja95@users.noreply.github.com>
2026-08-27 00:41:37 +0530

    feat: add glue catalog and etl job

---
+++ terraform/modules/data-collection/glue/main.tf
@@ -0,0 +1,2 @@
+resource "aws_glue_catalog_database" "this" { name=replace("${var.name_prefix}_catalog","-","_") }
+resource "aws_glue_job" "transform" { name="${var.name_prefix}-glue-transform" role_arn=var.data_role_arn glue_version="4.0" worker_type="G.1X" number_of_workers=2 command { name="glueetl" script_location="s3://${var.artifacts_bucket_name}/glue/glue_transform.py" python_version="3" } default_arguments={"--job-language"="python","--RAW_PATH"="s3://${var.raw_bucket_name}/bike-sharing/","--CURATED_PATH"="s3://${var.curated_bucket_name}/bike-sharing/"} tags=var.tags }
